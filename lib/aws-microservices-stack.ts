b1d138846032ee5131f82d57cd05797d4829fd10
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2023-10-02 09:42:47 +0300

    create product microservice

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,16 +1,59 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import path = require('path');
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsMicroservicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productTable = new Table(this, 'product', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      tableName: 'product',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsMicroservicesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: [
+          'aws-sdk',
+        ]
+      },
+      environment: {
+        PRIMARY_KEY: 'id',
+        DYNAMODB_TABLE_NAME: productTable.tableName
+      },
+      runtime: Runtime.NODEJS_18_X,
+    };
+
+    const productFunction = new NodejsFunction(this, 'productLambdaFunction', {
+      entry: path.join(__dirname, `/../src/product/index.js`),
+      ...nodeJsFunctionProps,
+    });
+
+    productTable.grantReadWriteData(productFunction);
+
+    const apigw = new LambdaRestApi(this, 'productApi', {
+      restApiName: 'Product Service',
+      handler: productFunction,
+      proxy: false
+    });
+
+    const product = apigw.root.addResource('product');
+    product.addMethod('GET'); //GET /product
+    product.addMethod('POST');//POST /product
+
+    const singleProduct = product.addResource('{id}'); // /product/{id}
+    singleProduct.addMethod('GET');
+    singleProduct.addMethod('PUT');
+    singleProduct.addMethod('DELETE');
   }
 }
